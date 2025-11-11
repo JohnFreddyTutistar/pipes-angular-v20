aa5285f730bf58cff151f085f8d107fa5b44779a
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-11-10 23:39:22 -0500

    Pipes personalizados

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { ToogleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToogleCasePipe],
+  imports: [ToogleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
   styleUrl: './custom-page.component.css',
 })
 export default class CustomPageComponent {
 
   name = signal('John Freddy Tutistar');
+  nameButton = signal('ToggleCase')
 
   upperCase = signal(false)
 
